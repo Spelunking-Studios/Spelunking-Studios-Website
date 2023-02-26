962919dabe58ff10f0f13a3517e668c473761d42
Deven McWhirk <deven.mcwhirk@gmail.com>
2023-02-26 01:28:46 -0800

    roughly navbar, getting used to nextjs13

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import "styles/reset.css";
 import "styles/global.css";
+import NavBar from "./navbar";
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <title>Spelunking Studios</title>
             <body>
+                <NavBar/>
                 {children}
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import Link from "next/link"
-
 export default function Page() {
     return (
         <div>
             <h1>Test</h1>
             <p>Test page</p>
-            <Link href="/another_page">Go Here</Link>
         </div>
     );
 }
