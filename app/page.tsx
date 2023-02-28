8fe36bebeb9b01865bfd0c7ed10588b7a3ff7ab9
DevBro123 <deven.mcwhirk@gmail.com>
2023-02-27 22:59:23 -0800

    added images, improved pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import bg from "./placeholder.jpg";
-
+import title from "./SpelunkingStudiosTitleWhite.png";
 
 
 export default function Page() {
     return (
-        <div>
+        <div id="firstSectionBG">
             <div className="section" id="firstSection">
                 <div>
-                    <h1 className="text">Test</h1>
-                    <p className="text">Test page</p>
+                    <Image id="logoTitle" width="600" src={title} alt="helo"/>
+                    <p className="text">We're high schoolers that make games</p>
                 </div>                
             </div>
         </div>
