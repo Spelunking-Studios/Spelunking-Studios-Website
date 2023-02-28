537eb90c81fda0408d5ab5cf4d4211a717f759db
DevBro123 <deven.mcwhirk@gmail.com>
2023-02-28 00:42:48 -0800

    improved first page, working on navbar

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -3,6 +3,20 @@ import Image from "next/image"
 import banner from "app/SpelunkingStudiosBannerWhite.png"
 
 export default function NavBar(){
+    
+    /*if(typeof window !== "undefined"){
+        const pathMap = {
+            '/games': document.getElementById('nav_games')?.style,
+            '/about': document.getElementById('nav_about')?.style, 
+            '/contact_us': document.getElementById('nav_contact')?.style, 
+            '/account': document.getElementById('nav_account')?.style
+        }
+
+        if(pathMap[location.pathname]){
+            pathMap[location.pathname].setProperty('--underline-active', '100%');
+        }
+    }*/
+
     return(
         <nav>
             <div id="navContents">
@@ -10,10 +24,10 @@ export default function NavBar(){
                     <Image src={banner} width={250} alt="Spelunking Studios"/>
                 </a>
                 <div id="navLinks">
-                    <Link className="link" href="/games">Games</Link>
-                    <Link className="link" href="/about">About</Link>
-                    <Link className="link" href="/contact_us">Contact Us</Link>
-                    <Link className="link" href="/account">Account</Link>
+                    <Link id="nav_games"  className="link" href="/games">Games</Link>
+                    <Link id="nav_about" className="link" href="/about">About</Link>
+                    <Link id="nav_contact" className="link" href="/contact_us">Contact Us</Link>
+                    <Link id="nav_account" className="link" href="/account">Account</Link>
                 </div>
             </div>
         </nav>
