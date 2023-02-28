8113ed0beb7dad0cd63930508fc25baaf9854fe5
DevBro123 <deven.mcwhirk@gmail.com>
2023-02-27 21:58:03 -0800

    Changed navbar, added pages

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,15 +1,12 @@
-import Image from "next/image";
-import bg from "./placeholder.jpg";
-
-
+import Link from "next/link";
 
 export default function Page() {
     return (
         <div>
             <div className="section" id="firstSection">
                 <div>
-                    <h1 className="text">Test</h1>
-                    <p className="text">Test page</p>
+                    <h1 className="text">Account</h1>
+                    <p className="text">Account settings</p>
                 </div>                
             </div>
         </div>
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link"
 import Image from "next/image"
-import banner from "./SpelunkingStudiosBanner.png"
+import banner from "app/SpelunkingStudiosBanner.png"
 
 export default function NavBar(){
     return(
         <nav>
             <div id="navContents">
-                <Image src={banner} width={250} alt="Spelunking Studios"/>
+                <a href="/">
+                    <Image src={banner} width={250} alt="Spelunking Studios"/>
+                </a>
                 <div id="navLinks">
-                    <Link className="link" href="/another_page">Games</Link>
-                    <Link className="link" href="#">About</Link>
-                    <Link className="link" href="#">Account</Link>
+                    <Link className="link" href="/games">Games</Link>
+                    <Link className="link" href="/about">About</Link>
+                    <Link className="link" href="/contact_us">Contact Us</Link>
+                    <Link className="link" href="/account">Account</Link>
                 </div>
             </div>
         </nav>
