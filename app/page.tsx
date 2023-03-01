e53b13d5cddb037ae23cbe2006daf1d3be46d600
DevBro123 <deven.mcwhirk@gmail.com>
2023-03-01 01:14:08 -0800

    fixed apostrophe thing

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
             <div className="section" id="firstSection">
                 <div>
                     <Image src={title} alt="helo" width={"600"} style={{objectFit: "contain"}}/>
-                    <p className="text">We're high schoolers that make games</p>
+                    <p className="text">We&apos;re high schoolers that make games</p>
                 </div>                
             </div>
         </div>
