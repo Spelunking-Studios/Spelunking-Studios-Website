69e4077b7912ea3bfcb77c59104b01ec0ccacd18
DevBro123 <deven.mcwhirk@gmail.com>
2023-03-02 00:54:24 -0800

    mobile hamburger mmm yummy

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,35 +1,45 @@
+"use client";
 import Link from "next/link"
 import Image from "next/image"
 import banner from "app/SpelunkingStudiosBannerWhite.png"
+import { useState } from "react"
+import { useRouter } from "next/router";
 
 export default function NavBar(){
-    
-    /*if(typeof window !== "undefined"){
-        const pathMap = {
-            '/games': document.getElementById('nav_games')?.style,
-            '/about': document.getElementById('nav_about')?.style, 
-            '/contact_us': document.getElementById('nav_contact')?.style, 
-            '/account': document.getElementById('nav_account')?.style
-        }
+    const [mobileLinksShown, setMobileLinksShown] = useState(false);
+    const router = useRouter();
 
-        if(pathMap[location.pathname]){
-            pathMap[location.pathname].setProperty('--underline-active', '100%');
-        }
-    }*/
+    function toggleMobile(){
+        setMobileLinksShown(!mobileLinksShown);
+        console.log("hello");
+    }
 
     return(
-        <nav>
-            <div id="navContents">
-                <a href="/">
-                    <Image src={banner} width={250} alt="Spelunking Studios" style={{objectFit: "contain"}}/>
-                </a>
-                <div id="navLinks">
-                    <Link id="nav_games"  className="link" href="/games">Games</Link>
-                    <Link id="nav_about" className="link" href="/about">About</Link>
-                    <Link id="nav_contact" className="link" href="/contact_us">Contact Us</Link>
-                    <Link id="nav_account" className="link" href="/account">Account</Link>
+        <div>
+            <nav>
+                <div id="navContents">
+                    <a href="/">
+                        <Image src={banner} width={250} alt="Spelunking Studios" style={{objectFit: "contain"}}/>
+                    </a>
+                    <div id="navLinks">
+                        <Link id="nav_games" className="link" href="/games">Games</Link>
+                        <Link id="nav_about" className="link" href="/about">About</Link>
+                        <Link id="nav_contact" className="link" href="/contact_us">Contact Us</Link>
+                        <Link id="nav_account" className="link" href="/account">Account</Link>
+                    </div>
                 </div>
-            </div>
-        </nav>
+                <div id="mobContainer" style={{display: mobileLinksShown ? "flex" : "none", opacity: mobileLinksShown ? "100%" : "0%"}}>
+                    <div id="mobileLinks">
+                        <Link id="nav_games" className="link mobile" href="/games" onClick={toggleMobile}>Games</Link>
+                        <Link id="nav_about" className="link mobile" href="/about" onClick={toggleMobile}>About</Link>
+                        <Link id="nav_contact" className="link mobile" href="/contact_us" onClick={toggleMobile}>Contact Us</Link>
+                        <Link id="nav_account" className="link mobile" href="/account" onClick={toggleMobile}>Account</Link>
+                    </div>
+                </div>
+                
+                <i className="material-symbols-outlined" id="mobileHamburger" onClick={toggleMobile}>{mobileLinksShown ? "close" : "menu"}</i>
+            </nav>
+        </div>
+        
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,12 @@ export default function Page() {
                     <p className="text">We&apos;re high schoolers that make games</p>
                 </div>                
             </div>
+            <div className="section" id="firstSection">
+                <div>
+                    <Image src={title} alt="helo" width={"600"} style={{objectFit: "contain"}}/>
+                    <p className="text">We&apos;re high schoolers that make games</p>
+                </div>                
+            </div>
         </div>
     );
 }
