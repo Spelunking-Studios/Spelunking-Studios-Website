5adfd2ca1c01bd649aec9fd54e10b8fba179ecd9
DevBro123 <deven.mcwhirk@gmail.com>
2023-03-03 02:07:19 -0800

    animated mobile nav

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -17,7 +17,7 @@ export default function NavBar(){
             <nav>
                 <div id="navContents">
                     <a href="/">
-                        <Image src={banner} width={250} alt="Spelunking Studios" style={{objectFit: "contain"}}/>
+                        <Image id="banner" src={banner} width={250} alt="Spelunking Studios" style={{objectFit: "contain"}}/>
                     </a>
                     <div id="navLinks">
                         <Link id="nav_games" className="link" href="/games">Games</Link>
@@ -26,12 +26,12 @@ export default function NavBar(){
                         <Link id="nav_account" className="link" href="/account">Account</Link>
                     </div>
                 </div>
-                <div id="mobContainer" style={{display: mobileLinksShown ? "flex" : "none", opacity: mobileLinksShown ? "100%" : "0%"}}>
+                <div id="mobContainer" style={{animation: mobileLinksShown ? "open .25s ease-in-out forwards" : "close .25s ease-in-out forwards"}}>
                     <div id="mobileLinks">
-                        <Link id="nav_games" className="link mobile" href="/games" onClick={toggleMobile}>Games</Link>
-                        <Link id="nav_about" className="link mobile" href="/about" onClick={toggleMobile}>About</Link>
-                        <Link id="nav_contact" className="link mobile" href="/contact_us" onClick={toggleMobile}>Contact Us</Link>
-                        <Link id="nav_account" className="link mobile" href="/account" onClick={toggleMobile}>Account</Link>
+                        <Link id="nav_games" className="link mobile" href="/games" onClick={toggleMobile} style={{animation: mobileLinksShown ? "appear .6s ease-in-out forwards" : "none"}}>Games</Link>
+                        <Link id="nav_about" className="link mobile" href="/about" onClick={toggleMobile} style={{animation: mobileLinksShown ? "appear .6s ease-in-out .1s forwards" : "none"}}>About</Link>
+                        <Link id="nav_contact" className="link mobile" href="/contact_us" onClick={toggleMobile} style={{animation: mobileLinksShown ? "appear .6s ease-in-out .2s forwards" : "none"}}>Contact Us</Link>
+                        <Link id="nav_account" className="link mobile" href="/account" onClick={toggleMobile} style={{animation: mobileLinksShown ? "appear .6s ease-in-out .3s forwards" : "none"}}>Account</Link>
                     </div>
                 </div>
                 
