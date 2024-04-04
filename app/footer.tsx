1c53ae410131a7d8bbdcc1a112867d10bd9e8d2b
DevBro123 <deven.mcwhirk@gmail.com>
2024-04-04 00:40:55 -0700

    update banner directory

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import banner from "app/SpelunkingStudiosBannerWhite.png";
+import banner from "public/images/SpelunkingStudiosBannerWhite.png";
 import { useState, useEffect } from "react";
 
 export default function Footer() {
