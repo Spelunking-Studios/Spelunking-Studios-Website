218aa176d03bb5750c7c3a200d631f767928432e
DevBro123 <deven.mcwhirk@gmail.com>
2024-04-04 00:31:20 -0700

    Begun footer implementation

--- app/page.tsx
+++ app/page.tsx
@@ -18,19 +18,6 @@ export default function Page() {
                     </p>
                 </div>
             </div>
-            <div className="section" id="firstSection">
-                <div>
-                    <Image
-                        src={title}
-                        alt="helo"
-                        width={"600"}
-                        style={{ objectFit: "contain" }}
-                    />
-                    <p className="text">
-                        We&apos;re high schoolers that make games
-                    </p>
-                </div>
-            </div>
         </div>
     );
 }
