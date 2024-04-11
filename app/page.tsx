1230ad2d5f0a366cb2295e0d586d80691d9fd8bc
DevBro123 <deven.mcwhirk@gmail.com>
2024-04-10 23:22:48 -0700

    fjesoifjesfos

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,11 @@ export default function Page() {
                         src={title}
                         alt="helo"
                         width={"600"}
-                        style={{ objectFit: "contain" }}
+                        style={{ objectFit: "contain", margin: "auto" }}
                     />
-                    <p className="text">
-                        We&apos;re high schoolers that supposedly want to work on a game, or something, so far we've done nothing
+                    <p className="text"
+                        style={{ width: "680pt", margin: "auto" }}>
+                        We&apos;re high schoolers that supposedly want to work on a game, or something, so far we&apos;ve done nothing
                     </p>
                 </div>
             </div>
